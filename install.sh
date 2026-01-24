5ec59903f407ee57d1afe3adfe5b8ced186ec28c
Sl (Shahaf Levi) <admin@slsrepo.com>
2026-01-25 00:23:53 +0200

    Add Python virtual environment with textual, to avoid Arch Python upgrade issues

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ pacman -Sy --noconfirm archlinux-keyring
 pacman -Sy --noconfirm python-textual
 curl -fsSL -o t2archinstall.py https://github.com/slsrepo/t2archinstall/raw/refs/heads/main/t2archinstall.py
 chmod +x t2archinstall.py
+python3 -m venv ~
+bin/pip install textual
